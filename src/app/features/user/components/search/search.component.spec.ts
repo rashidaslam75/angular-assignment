e893eb5470d020ddefe3ad3764babe2b951860bd
Rashid Aslam <rashid@aiondigital.com>
2022-06-11 21:45:52 +0500

    feat: test cases issues resolved

--- src/app/features/user/components/search/search.component.spec.ts
+++ src/app/features/user/components/search/search.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserService } from 'src/app/services/user/user.service';
 
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-
+  let userServiceMock: any;
+  
   beforeEach(async () => {
+    userServiceMock = jasmine.createSpyObj('UserService', [
+      'onSubmit',
+  ]);
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      providers:[
+        {
+          provide: UserService,
+          useValue: userServiceMock,
+      },
+      ]
     })
     .compileComponents();
   });
--- src/app/services/network/network.service.spec.ts
+++ src/app/services/network/network.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NetworkService } from './network.service';
 
 describe('NetworkService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+  }));
 
   it('should be created', () => {
     const service: NetworkService = TestBed.get(NetworkService);
